23ce0c93af6c4854d57316592ca072213addbefb
Susanto <sushantomahato1@gmail.com>
2025-02-13 17:42:45 +0530

    feat: mongodb connection established

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.listen(5001, () => {
-  console.log("Server is running in port 5001");
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`);
+  connectDB();
 });
