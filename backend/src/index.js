2e3fd0b1873e18dd0fdbd0578b13a7e37ff49b98
Susanto <sushantomahato1@gmail.com>
2025-02-14 02:56:46 +0530

    feat: apis foe messaging added

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -14,6 +15,7 @@ app.use(express.json()); // allows to extract JSON data out of request body
 app.use(cookieParser()); // allows to read the cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running in port ${PORT}`);
